74c796934ecb6aeee0b5b2bed29e52d4cfcd2cb6
Tchinda-Azice <159732891+Tchinda-Azice@users.noreply.github.com>
2024-02-25 01:41:49 +0100

    Update hi.js

--- scripts/cmds/hi.js
+++ scripts/cmds/hi.js
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "hi") return message.reply("hello Bro, how can i help you? Type -Tkgc✨ to join my supportgc🤲🚀🥺");
+    if (event.body && event.body.toLowerCase() == "hi") return message.reply("hello Bro, how can i help you? Type @Tkgc✨ to join my supportgc🤲🚀🥺");
 }
 };
